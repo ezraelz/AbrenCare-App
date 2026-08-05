e8589dbb14b25844d6fb6bf72013f1a6bfb8c44b
esrael <esraelzerihun50@gmail.com>
2026-08-05 11:29:12 +0300

    Ignore Backend directory

--- Backend/roles/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('roles', views.RoleView.as_view(), name='view-roles'),
-    path('roles/<int:pk>/', views.RoleDetailView.as_view(), name='role-detail')
-]
